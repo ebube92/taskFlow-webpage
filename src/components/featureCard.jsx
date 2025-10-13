60ff29457a489a1a2189e784d5b6ce860eb7296e
ebube <ebubeokafor02@gmail.com>
2025-10-13 16:24:54 +0100

    done with testimonial section

--- src/components/featureCard.jsx
+++ src/components/featureCard.jsx
@@ -1,6 +1,6 @@
 export default function FeatureCard({ icon, title, description }) {
   return (
-    <div className="bg-white flex flex-col font-outfit p-8 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 hover:scale-105 cursor-pointer shadow-gray-300 flex-1">
+    <div className="bg-white flex flex-col font-outfit p-8 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 shadow-gray-300 flex-1">
       <div className="bg-gray-200 p-3 rounded-xl mb-4 w-max h-max">
         <img src={icon} alt={title + " icon"} className="w-10 h-10"></img>
       </div>
--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -26,9 +26,9 @@ const testimonials = [
 export default function Testimonials() {
     return (
         <div className="w-full bg-gray-50">
-            <section className="min-h-[50vh] px-4 lg:px-16 w-full lg:w-[70%] mx-auto py-10 font-outfit flex flex-col items-center">
+            <section className="min-h-[50vh] px-6 lg:px-16 w-full lg:w-[70%] mx-auto py-10 font-outfit flex flex-col items-center">
                 <h2 className="text-4xl font-bold text-center">What our users say</h2>
-                <div className="flex flex-col md:flex-row gap-6 justify-between items-stretch mt-6 px-4 md:px-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-between items-stretch mt-6 px-4 w-full">
                     {testimonials.map((item, index) => (
                         <TestimonialCard key={index} {...item} />
                     ))}
